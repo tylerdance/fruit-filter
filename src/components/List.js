e76aa678b66251ec66878ff6e87d7698c21b330d
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2020-12-10 11:41:48 -0500

    created List class component

---
+++ src/components/List.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+
+        }
+    }
+    
+
+    render() {
+        return(
+            <div>
+                <h1>List</h1>
+            </div>
+        )
+    }
+}
+
+export default List;
