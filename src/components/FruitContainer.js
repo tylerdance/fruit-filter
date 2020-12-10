4f6bf99a1dc3e093c63133d162f23f7660ace777
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2020-12-10 12:10:59 -0500

    FruitContainer class component

---
+++ src/components/FruitContainer.js
@@ -0,0 +1,46 @@
+import React, { Component, useEffect } from 'react';
+import List from './List'
+import Input from './Input'
+
+class FruitContainer extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            // initialize the fruit list to the full list passed into props
+            // initialize filter value to an empty string
+            fruitsToDisplay: this.props.fruits,
+            filterValue: '',
+        }
+    }
+    
+    handleFilterChange = (e) => {
+        e.prevent.default();
+        let filterValue = e.target.value;
+        // remove fruits that dont contain the filter value
+        const filterFruitList = this.props.fruits.filter(fruit => {
+            // return fruit.toLowerCase().includes(filterValue.toLowerCase())
+
+            if (fruit.toLowerCase() === filterValue.toLowerCase()) {
+                return true;
+            }
+        })
+
+        this.setState({ 
+            fruitsToDisplay: filteredFruitList,
+            filterValue,
+        })
+    }
+
+    render() {
+        // Inside of Input component, to access props, I will say props.value
+        // Inside of List component, to access props, I will say props.fruits
+        return(
+            <div>
+                <Input value={this.state.filterValue} onChange={this.handleFilterChange}/>
+                <List fruits={this.state.fruitsToDisplay} />
+            </div>
+        )
+    }
+}
+
+export default FruitContainer;
